000985f510df5fec3d71d6f2aa696516bc62e6c5
cyt <992306784@qq.com>
2018-12-17 19:23:11 +0800

    <feat>:新增M-SP算法
    新增以下函数：
          M_SP.m           改善的SP算法，利用了前一个支撑集的信息
          M_SP_measure.m   将M-SP算法应用于MIMO
    修改了函数mainPro.m：
          加入了对M-SP算法的仿真

--- mainPro.m
+++ mainPro.m
@@ -12,31 +12,40 @@ NMSE_omp = zeros(1,len);   % omp
 NBG_omp = zeros(1,len);
 NMSE_CSA = zeros(1,len);   % CS_aided
 NBG_CSA = zeros(1,len); 
+NMSE_MSP = zeros(1,len);   % M-SP
+NBG_MSP = zeros(1,len);
 
 for i = 1 : length(Tp)
     [NMSE_GA(i),NBG_GA(i)] = Genie_aided(M,k,ks,Tp(i),SNR);
     [NMSE_SLS(i),NBG_SLS(i)] = StaticLS(M,k,ks,Tp(i),SNR);
     [NMSE_omp(i),NBG_omp(i)] = omp_measure(M,k,ks,Tp(i),SNR);
     [NMSE_CSA(i),NBG_CSA(i)] = CS_aided(M,k,ks,Tp(i),SNR);
+    [NMSE_MSP(i),NBG_MSP(i)] = M_SP_measure(M,k,ks,Tp(i),SNR);
 end
 
 figure(1)
 plot(Tp,NMSE_GA,'*-',Tp,NMSE_SLS,'d-',...
-    Tp,NMSE_omp,'>-',Tp,NMSE_CSA,'o-')
+    Tp,NMSE_omp,'>-',Tp,NMSE_CSA,'o-',...
+    Tp,NMSE_MSP,'h-')
 axis([40 100 0 0.5])
 grid on
 xlabel('Length of training sequence T_p')
 ylabel('Normalized mean squared error(NMSE)')
-legend('Genie-aided LS','Static LS','OMP','CS-aided')
+legend('Genie-aided LS','Static LS','OMP','CS-aided','M-SP')
+title({'NMSE versus the length of the training sequence with M=100,',...
+    'k=40,...ks=3,and SNR=20dB'});
 
 figure(2)
 plot(Tp,NBG_GA,'*-',Tp,NBG_SLS,'d-',...
-    Tp,NBG_omp,'>-',Tp,NBG_CSA,'o-')
+    Tp,NBG_omp,'>-',Tp,NBG_CSA,'o-',...
+    Tp,NBG_MSP,'h-')
 axis([40 100 0 1])
 grid on
 xlabel('Length of training sequence T_p')
 ylabel('Nromalized beamforming gain')
-legend('Genie-aided LS','Static LS','OMP','CS-aided')
+legend('Genie-aided LS','Static LS','OMP','CS-aided','M-SP')
+title({'Normalized beamforming gain versus the length of the training,',...
+    'sequence with M=100,k=40,...ks=3,and SNR=20dB'});
 
 Tp = 60;
 SNR = 0:5:30;
@@ -46,12 +55,15 @@ for i = 1:len
     [NMSE_SLS(i),NBG_SLS(i)] = StaticLS(M,k,ks,Tp,SNR(i));
     [NMSE_omp(i),NBG_omp(i)] = omp_measure(M,k,ks,Tp,SNR(i));
     [NMSE_CSA(i),NBG_CSA(i)] = CS_aided(M,k,ks,Tp,SNR(i));
+    [NMSE_MSP(i),NBG_MSP(i)] = M_SP_measure(M,k,ks,Tp,SNR(i));
 end
 figure(3)
 plot(SNR,NMSE_GA,'*-',SNR,NMSE_SLS,'d-',...
-    SNR,NMSE_omp,'>-',SNR,NMSE_CSA,'o-')
+    SNR,NMSE_omp,'>-',SNR,NMSE_CSA,'o-',...
+    SNR,NMSE_MSP,'h-')
 axis([0 30 0 1])
 grid on
 xlabel('SNR(dB)')
 ylabel('Normalized mean squared error(NMSE)')
-legend('Genie-aided LS','Static LS','OMP','CS-aided')
+legend('Genie-aided LS','Static LS','OMP','CS-aided','M-SP')
+title({'NMSE versus SNR with M=100,k=40,ks=3,and Tp=60'})
